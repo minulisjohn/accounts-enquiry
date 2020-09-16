37118fc55cbdee169192f4a530653e029d0d0356
minulisjohn <71218063+minulisjohn@users.noreply.github.com>
2020-09-16 19:48:32 +1000

    Initial commit

---
+++ accounts-enquiry/src/main/java/com/igreendata/accounts/AccountsEnquiryApplication.java
@@ -0,0 +1,13 @@
+package com.igreendata.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsEnquiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsEnquiryApplication.class, args);
+	}
+
+}
